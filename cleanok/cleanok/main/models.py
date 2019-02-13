d2bd4d9b51414ce521bf0e30167338e711861e24
Антохер <malyarov99@mail.ru>
2019-02-14 04:28:24 +1000

    Антохер
    +функция группировки

--- cleanok/cleanok/main/models.py
+++ cleanok/cleanok/main/models.py
@@ -8,10 +8,20 @@ class ShortInfoAboutCompany(models.Model):
 
 	text_about = models.TextField()
 
+	class Meta:
+
+		verbose_name = 'Краткая информация о компании'
+		verbose_name_plural = 'Краткая информация о компании'
+
 class InfoItem(models.Model):
 
 	info_name = models.CharField(max_length=20, unique=True)
 	info_description = models.TextField()
+
+	class Meta:
+
+		verbose_name = 'Пункт информации'
+		verbose_name_plural = 'Пукнты информации'
 
 # Блок "Услуги"
 
@@ -21,20 +31,59 @@ class Service(models.Model):
 	short_desc = models.TextField()
 	description = models.TextField()
 
+	class Meta:
+
+		verbose_name = 'Услуга'
+		verbose_name_plural = 'Услуги'
+
 class ServiceWorksNames(models.Model):
 
 	service_name = models.ForeignKey(Service, to_field='name', on_delete=models.CASCADE)
 	work_title = models.CharField(max_length=50)
 	min_price = models.SmallIntegerField()
 
+	class Meta:
+
+		verbose_name = 'Наименование работы'
+		verbose_name_plural = 'Наименование работ'
+
+class SpecialService(models.Model):
+
+	name = models.CharField(max_length=40, unique=True)
+	short_desc = models.TextField()
+	description = models.TextField()
+
+	class Meta:
+
+		verbose_name = 'Спец. Услуга'
+		verbose_name_plural = 'Спец. Услуги'
+
+class SpecialServiceWorksNames(models.Model):
+
+	service_name = models.ForeignKey(SpecialService, to_field='name', on_delete=models.CASCADE)
+	work_title = models.CharField(max_length=50)
+	min_price = models.SmallIntegerField()
+	count = models.SmallIntegerField()
+	units = models.CharField(max_length=10)
+
+	class Meta:
+
+		verbose_name = 'Наименование спец. работы'
+		verbose_name_plural = 'Наименование спец. работ'
+
 # Блок "Почему мы"
 
 class WhyWe(models.Model):
 
 	name = models.CharField(max_length=30)
-	image_for_name = models.ImageField(upload_to="uploads/images/")
+	#image_for_name = models.ImageField(upload_to="uploads/images/")
 	description = models.TextField()
-	image_for_description = models.ImageField(upload_to="uploads/images/")
+	#image_for_description = models.ImageField(upload_to="uploads/images/")
+
+	class Meta:
+
+		verbose_name = 'Почему мы'
+		verbose_name_plural = 'Почему мы'
 
 # Блок "Как мы работаем"
 
@@ -44,10 +93,20 @@ class OrderRequest(models.Model):
 	phone = models.CharField(max_length=20)
 	email = models.CharField(max_length=20)
 
+	class Meta:
+
+		verbose_name = 'Заказ'
+		verbose_name_plural = 'Заказы'
+
 # Блок "Отзывы"
 
 class Reviews(models.Model):
 
 	name = models.CharField(max_length=35)
-	reviewer_photo = models.ImageField(upload_to="uploads/images/")
+	#reviewer_photo = models.ImageField(upload_to="uploads/images/")
 	review_text = models.TextField()
+
+	class Meta:
+
+		verbose_name = 'Отзыв'
+		verbose_name_plural = 'Отзывы'
--- cleanok/cleanok/main/views.py
+++ cleanok/cleanok/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from main.models import *
-
+from collections import defaultdict
 
 # Create your views here.
 
@@ -12,8 +12,21 @@ def index(request):
 
 	qs_service = Service.objects.all()
 
-	# qs_whywe = WhyWe.objects.all()
+	qs_worksnames = ServiceWorksNames.objects.all()
+
+	combined_worknames = combine_items(qs_worksnames)
+
+	print(combined_worknames)
 
 	return render(request, 'main.html', {'short_info': qs_short_info, 'info_item': qs_info_item, 
-										  'service': qs_service, # 'whywe': qs_whywe
+										  'service': qs_service, 'combined': combined_worknames
 										})
+
+def combine_items(q_set):
+
+	combined = defaultdict(list)
+
+	for item in q_set:
+		combined[item.min_price].append(item.work_title)
+
+	return combined
