43aea5fdc154ea3076a28784fd9ffe7bd05f1d8b
Антохер <malyarov99@mail.ru>
2019-02-25 05:59:27 +1000

    edited flatpage

--- cleanok/cleanok/flatpage_main/models.py
+++ cleanok/cleanok/flatpage_main/models.py
@@ -1,2 +1,9 @@
 from django.db import models
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
 
+
+class MyFlatPage(FlatPage):
+    
+    description = models.TextField(_('Description'), blank=True)
+    desc_items = models.TextField(_('Description items'), blank=True)
